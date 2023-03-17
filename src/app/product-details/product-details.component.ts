7548198ba45e6740507c9a13f11408b31e00dd17
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-17 13:31:51 +0000

    kekw

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+
+  product: Product | undefined;
+
+  constructor(private route : ActivatedRoute) {}
+
+}
