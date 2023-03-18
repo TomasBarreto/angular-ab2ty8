6cd7a7963bf230f478846c8919e6446d01cbe2ac
TOXIK523 <65412084+TOXIK523@users.noreply.github.com>
2023-03-18 07:49:18 +0000

    more

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../app/products';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   items: Product[] = [];
 
@@ -19,5 +24,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
